b57ab63c80a5c2a67f26726c2ccec710f4527e5b
Zhipeng-Chang <zchang0302@gmail.com>
2018-09-20 17:20:50 -0600

    done for today

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ from httplib2 import Http
 from oauth2client.service_account import ServiceAccountCredentials
 from difflib import SequenceMatcher
 
-SIMILAR_RATE = 0.5
+SIMILAR_RATE = 0.6
 
 app = Flask(__name__)
 
@@ -128,29 +128,32 @@ def create_card_response(verb_null_string,parsed_string,event_message,user_name)
         eventMessage: the user's message to the bot
 
     """
-    
-    if event_message.lower() in rules.CHEER_LIST:
-        text = ("Hey! "+user_name+" Thank you for talking to Chatbot about Chatbot :D You can ask me Chatbot type, opportunities, use cases in industry,"
-        "municipal government, or at the City of Edmonton, my recommendations and the next steps.")
-        headertitle = 'City of Edmonton chatbot'
-        headerimage = 'http://www.gwcl.ca/wp-content/uploads/2014/01/IMG_4371.png'
-        widgetimage = 'https://media1.tenor.com/images/9ea72ef078139ced289852e8a4ea0c5c/tenor.gif?itemid=7537923'
-        return cardsFactory._text_card_with_image(headertitle, headerimage,text, widgetimage)
+    for word in rules.CHEER_LIST:
+        if similar(event_message, word)>=SIMILAR_RATE:
+            text = ("Hey! "+user_name+" Thank you for talking to Chatbot about Chatbot :D You can ask me Chatbot type, opportunities, use cases in industry,"
+            " municipal government, or at the City of Edmonton, my recommendations and the next steps.")
+            headertitle = 'City of Edmonton chatbot'
+            headerimage = 'http://www.gwcl.ca/wp-content/uploads/2014/01/IMG_4371.png'
+            widgetimage = 'https://media1.tenor.com/images/9ea72ef078139ced289852e8a4ea0c5c/tenor.gif?itemid=7537923'
+            return cardsFactory._text_card_with_image(headertitle, headerimage,text, widgetimage)
 
-
-    elif event_message.lower() in rules.BYE_LIST:
-        text = 'Bye~ Thank you very much for chatting with me. Hope the information provided is helpful. Have a nice day!'
-        headertitle = 'City of Edmonton chatbot'
-        headerimage = 'http://www.gwcl.ca/wp-content/uploads/2014/01/IMG_4371.png'
-        widgetimage = 'https://img.buzzfeed.com/buzzfeed-static/static/2017-01/17/16/asset/buzzfeed-prod-fastlane-01/anigif_sub-buzz-20527-1484687195-4.gif'
-        return cardsFactory._text_card_with_image(headertitle, headerimage,text, widgetimage)
+    for word in rules.BYE_LIST:
+        if similar(event_message, word)>=SIMILAR_RATE:
+            text = 'Bye~ Thank you very much for chatting with me. Hope the information provided is helpful. Or, you can leave your feedback here! Have a nice day!'
+            headertitle = 'City of Edmonton chatbot'
+            headerimage = 'http://www.gwcl.ca/wp-content/uploads/2014/01/IMG_4371.png'
+            widgetimage = 'https://img.buzzfeed.com/buzzfeed-static/static/2017-01/17/16/asset/buzzfeed-prod-fastlane-01/anigif_sub-buzz-20527-1484687195-4.gif'
+            return cardsFactory._text_card_with_image(headertitle, headerimage,text, widgetimage)
 
     
     else:
-        related_questions_list=search_highest_rate(verb_null_string)
+
+        related_questions_list=[]
+        related_questions_list=search_related_rate(verb_null_string)
     
         if (len(related_questions_list)==0):
-            text = 'Oops! No result found, please search again.'
+            text = 'I am afraid I am not able to understand and answer your question. I am still learning. Currently, I am only trained to answer questions about Chatbot type, opportunities, use cases in industry,\
+            municipal government, or at the City of Edmonton, my recommendations and the next steps.'
             headertitle = 'City of Edmonton chatbot'
             headerimage = 'http://www.gwcl.ca/wp-content/uploads/2014/01/IMG_4371.png'
             widgetimage = 'https://get.whotrades.com/u3/photo843E/20389222600-0/big.jpeg'
@@ -171,12 +174,12 @@ def create_card_response(verb_null_string,parsed_string,event_message,user_name)
             cards.append(header)
             
             for question in related_questions_list:
-                widgets.append({
-                    'buttons': [{'textButton': {'text': question,'onClick': {'action': {'actionMethodName': INTERACTIVE_TEXT_BUTTON_ACTION,'parameters': [{'key': INTERACTIVE_BUTTON_PARAMETER_KEY,'value': question
-                                        }]}}}}]
+                widgets.append(
+                {'buttons': [{'textButton': {'text': question,'onClick': {'action': {'actionMethodName': INTERACTIVE_TEXT_BUTTON_ACTION,'parameters': [{'key': INTERACTIVE_BUTTON_PARAMETER_KEY,'value': question}]}}}}]
                 })
             cards.append({ 'sections': [{ 'widgets': widgets }]})
             response['cards'] = cards
+
             return response
         
 
@@ -198,7 +201,8 @@ def respond_to_interactive_card_click(action_name, custom_params):
         theAnswer = rules.getTheAns(question)
         return theAnswer
     else:
-        text = 'Oops! No result found, please search again.'
+        text = 'I am afraid I am not able to understand and answer your question. I am still learning. Currently, I am only trained to answer questions about Chatbot type, opportunities, use cases in industry,\
+        municipal government, or at the City of Edmonton, my recommendations and the next steps.'
         headertitle = 'City of Edmonton chatbot'
         headerimage = 'http://www.gwcl.ca/wp-content/uploads/2014/01/IMG_4371.png'
         widgetimage = 'https://get.whotrades.com/u3/photo843E/20389222600-0/big.jpeg'
@@ -213,7 +217,7 @@ def search_highest_rate(verb_null_string):
     higest_rate = 0
     higest_question = None
     for each_question,each_answer in rules.QUESTION_DIC.items():
-        similar_rate = similar(each_question,verb_null_string)
+        similar_rate = similar(verb_null_string,each_question)
         if higest_rate == 0:
             higest_rate = similar_rate
             higest_question = each_question
@@ -221,13 +225,14 @@ def search_highest_rate(verb_null_string):
             if similar_rate>higest_rate:
                 higest_rate = similar_rate
                 higest_question = each_question
-    if higest_rate > 0.3:
+    if higest_rate > 0.35:
         related_questions_list.append(higest_question)
     return related_questions_list
 
 
 def search_related_rate(verb_null_string):
+    related_questions_list = []
     for each_question,each_answer in rules.QUESTION_DIC.items():
-        if similar(each_question,verb_null_string)>=SIMILAR_RATE:
+        if similar(verb_null_string,each_question)>=SIMILAR_RATE:
             related_questions_list.append(each_question)
     return related_questions_list
