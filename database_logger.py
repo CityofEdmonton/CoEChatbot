2cde8e3d1ea5fae1b6991a6f35290637d32b65d9
Zhipeng-Chang <zchang0302@gmail.com>
2018-12-20 08:40:51 -0700

    add try blcoks

--- database_logger.py
+++ database_logger.py
@@ -28,66 +28,91 @@ def connect_to_cloudsql():
 
 
 def logging_to_database(user,question,answer,parsed_key_words, search_used, group):
-    db = connect_to_cloudsql()
-    cursor = db.cursor()
-    cursor.execute("USE history")
-    tz = pytz.timezone('America/Edmonton')
-    timestamp = datetime.datetime.now(tz).isoformat()
-    user=str(user)
-    question=str(question)
-    answer = str(answer)
-    cursor.execute("delete from history.email_tem_table where TIMESTAMPDIFF(SECOND, Timestamp, now())>300")
-    cursor.execute("INSERT INTO chat_history VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", [timestamp, user, question, answer, "Null", parsed_key_words, search_used, group])
-    db.commit()
+    try:
+        db = connect_to_cloudsql()
+        cursor = db.cursor()
+        cursor.execute("USE history")
+        tz = pytz.timezone('America/Edmonton')
+        timestamp = datetime.datetime.now(tz).isoformat()
+        user=str(user)
+        question=str(question)
+        answer = str(answer)
+        cursor.execute("delete from history.email_tem_table where TIMESTAMPDIFF(SECOND, Timestamp, now())>300")
+        cursor.execute("INSERT INTO chat_history VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", [timestamp, user, question, answer, "Null", parsed_key_words, search_used, group])
+        db.commit()
+
+    except Exception as e:
+        print("error at logging_to_database: ", e)
+        pass
+
 
 
 def update_selected_answer(user, answer_selected):
-    db = connect_to_cloudsql()
-    cursor = db.cursor()
-    user=str(user)
-    answer_selected = str(answer_selected)
-    cursor.execute("SET SQL_SAFE_UPDATES = 0")
-    cursor.execute("delete from history.email_tem_table where TIMESTAMPDIFF(SECOND, Timestamp, now())>300")
-    cursor.execute("Update history.chat_history Set Answer_selected = %s where Timestamp = (SELECT Timestamp FROM (select * from history.chat_history) AS subtable where User = %s ORDER BY Timestamp DESC LIMIT 1) and User =%s", [answer_selected, user,  user])
-    db.commit()
+    try:
+        db = connect_to_cloudsql()
+        cursor = db.cursor()
+        user=str(user)
+        answer_selected = str(answer_selected)
+        cursor.execute("SET SQL_SAFE_UPDATES = 0")
+        cursor.execute("delete from history.email_tem_table where TIMESTAMPDIFF(SECOND, Timestamp, now())>300")
+        cursor.execute("Update history.chat_history Set Answer_selected = %s where Timestamp = (SELECT Timestamp FROM (select * from history.chat_history) AS subtable where User = %s ORDER BY Timestamp DESC LIMIT 1) and User =%s", [answer_selected, user,  user])
+        db.commit()
+
+    except Exception as e:
+        print("error at update_selected_answer: ", e)
+        pass
 
 
 def log_question_tem(user, question,respons):
-    db = connect_to_cloudsql()
-    cursor = db.cursor()
-    user=str(user)
-    question = str(question)
-    respons = str(respons)
-    cursor.execute("USE history")
-    cursor.execute("SET SQL_SAFE_UPDATES = 0")
-    cursor.execute("delete from history.email_tem_table where TIMESTAMPDIFF(SECOND, Timestamp, now())>300")
-    cursor.execute("INSERT INTO  history.email_tem_table(User, Questions, Respons) VALUES (%s, %s, %s)", [user, question, respons])
-    db.commit()
+    try:
+        db = connect_to_cloudsql()
+        cursor = db.cursor()
+        user=str(user)
+        question = str(question)
+        respons = str(respons)
+        cursor.execute("USE history")
+        cursor.execute("SET SQL_SAFE_UPDATES = 0")
+        cursor.execute("delete from history.email_tem_table where TIMESTAMPDIFF(SECOND, Timestamp, now())>300")
+        cursor.execute("INSERT INTO  history.email_tem_table(User, Questions, Respons) VALUES (%s, %s, %s)", [user, question, respons])
+        db.commit()
+    except Exception as e:
+        print("error at log_question_tem: ", e)
+        pass
 
 
 def check_log_question_tem(user):
-    db = connect_to_cloudsql()
-    cursor = db.cursor()
-    user=str(user)
-    cursor.execute("USE history")
-    cursor.execute("SET SQL_SAFE_UPDATES = 0")
-    cursor.execute("delete from history.email_tem_table where TIMESTAMPDIFF(SECOND, Timestamp, now())>300")
-    cursor.execute("select Questions, Respons from history.email_tem_table where User =%s", [user])
-    data =cursor.fetchone()
-    db.commit()
-    if data is not None:
-        question = data[0]
-        respons = data[1]
-        return question, respons
-    else:
-        return None, None
+    try:
+        db = connect_to_cloudsql()
+        cursor = db.cursor()
+        user=str(user)
+        cursor.execute("USE history")
+        cursor.execute("SET SQL_SAFE_UPDATES = 0")
+        cursor.execute("delete from history.email_tem_table where TIMESTAMPDIFF(SECOND, Timestamp, now())>300")
+        cursor.execute("select Questions, Respons from history.email_tem_table where User =%s", [user])
+        data =cursor.fetchone()
+        db.commit()
+        if data is not None:
+            question = data[0]
+            respons = data[1]
+            return question, respons
+        else:
+            return None, None
+
+    except Exception as e:
+        print("error at check_log_question_tem: ", e)
+        pass
 
 def delete_log_question_tem(user, question):
-    db = connect_to_cloudsql()
-    cursor = db.cursor()
-    user=str(user)
-    question = str(question)
-    cursor.execute("USE history")
-    cursor.execute("SET SQL_SAFE_UPDATES = 0")
-    cursor.execute("delete from history.email_tem_table where User = %s and Questions = %s", [user, question])
-    db.commit()
+    try:
+        db = connect_to_cloudsql()
+        cursor = db.cursor()
+        user=str(user)
+        question = str(question)
+        cursor.execute("USE history")
+        cursor.execute("SET SQL_SAFE_UPDATES = 0")
+        cursor.execute("delete from history.email_tem_table where User = %s and Questions = %s", [user, question])
+        db.commit()
+
+    except Exception as e:
+        print("error at delete_log_question_tem: ", e)
+        pass
